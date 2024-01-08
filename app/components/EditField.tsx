1abea500ba4c01093e8ee6d5a79909419af2b235
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-01-08 10:03:50 +0200

    add link of api

--- app/components/EditField.tsx
+++ app/components/EditField.tsx
@@ -37,7 +37,7 @@ const EditField = ({ sectionId, inputIndex }: EditFieldPropes) => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `http://localhost:5000/retrieve/${sectionId}`,
+          `https://kora-api-sfwh.onrender.com/retrieve/${sectionId}`,
         );
         const data = await response.json();
 
@@ -60,7 +60,7 @@ const EditField = ({ sectionId, inputIndex }: EditFieldPropes) => {
     const value = inputValue;
 
     try {
-      const response = await fetch("http://localhost:5000/submit", {
+      const response = await fetch("https://kora-api-sfwh.onrender.com/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -85,7 +85,7 @@ const EditField = ({ sectionId, inputIndex }: EditFieldPropes) => {
     const value = "";
 
     try {
-      const response = await fetch("http://localhost:5000/submit", {
+      const response = await fetch("https://kora-api-sfwh.onrender.com/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
