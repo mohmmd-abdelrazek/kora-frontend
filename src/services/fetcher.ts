540ccfdb642e8cd7bec868db123bb4656413c432
Mohamed <mohmmd.abdelrazek@gmail.com>
2024-02-28 09:16:42 +0200

    add next_pupoppppppppRangeError: Maximum call stack size exceeded

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: process.env.SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 });
 
 const fetcher = (url : string) => axiosInstance.get(url).then(res => res.data);
